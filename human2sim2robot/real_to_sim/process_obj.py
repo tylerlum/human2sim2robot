18572a5e26d9c8f87fc34d33e5cb20330db46281
Tyler Lum <tylergwlum@gmail.com>
2025-04-29 16:54:48 -0700

    Update process_obj.py

--- human2sim2robot/real_to_sim/process_obj.py
+++ human2sim2robot/real_to_sim/process_obj.py
@@ -90,24 +90,5 @@ def main() -> None:
         print(f"Created URDF at {output_urdf_path}")
 
 
-# INPUT_PATH = Path(
-#     "/afs/cs.stanford.edu/u/tylerlum/Downloads/plate_yellow_smiley/3DModel.obj"
-# )
-# OUTPUT_PATH = INPUT_PATH.parent / f"{INPUT_PATH.stem}_DEBUG.obj"
-# mesh = trimesh.load_mesh(INPUT_PATH)
-# print(f"INPUT_PATH = {INPUT_PATH}")
-#
-# T = np.eye(4)
-# T[:3, 3] = -mesh.centroid
-# print(f"mesh.centroid = {mesh.centroid}")
-# mesh.apply_transform(T)
-#
-# T2 = trimesh.transformations.rotation_matrix(np.pi / 2, [1, 0, 0])
-# mesh.apply_transform(T2)
-#
-# mesh.export(OUTPUT_PATH)
-# print(f"Saved to {OUTPUT_PATH}")
-
-
 if __name__ == "__main__":
     main()
